33bcf8c88c124fea34ade10e148f5af69e871ee9
htYum <1853201@tongji.edu.cn>
2020-08-28 13:56:56 +0800

    users

--- user_data/DBproject/Controllers/usersController.cs
+++ user_data/DBproject/Controllers/usersController.cs
@@ -30,17 +30,21 @@ namespace DBproject.Controllers
 
             var sAll = from m in dbContext.user_data
                        select m;
+            int num = sAll.Count();
             int pageNum = _in.page_num;
             int pageSize = _in.page_size;
 
             sAll = sAll.Skip((pageNum - 1) * pageSize).Take(pageSize);
             result.code = 1;
             result.message = "查询成功";
-            result.data = new user_data[pageSize];
+            result.data = new totalUsers();
+            result.data.total = num;
+            result.data.page_num = pageNum;
+            result.data.users = new user_data[pageSize];
             int i = 0;
             foreach(var p in sAll)
             {
-                result.data[i++] = p;
+                result.data.users[i++] = p;
             }
             return result;
         }
